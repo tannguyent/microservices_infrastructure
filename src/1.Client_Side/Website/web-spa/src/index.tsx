5a424b93952c7b6159562b9067e0b1d55d8e9a93
tan-tinh.nguyen <tan-tinh.nguyen@vn.devinition.com>
2019-04-15 18:10:05 +0700

    add some component into style source

--- src/1.Client_Side/Website/web-spa/src/index.tsx
+++ src/1.Client_Side/Website/web-spa/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 
 import App from './App';
 
-import '../styles/main.scss';
+import '../styles/style.scss';
 
 import registerServiceWorker from './registerServiceWorker';
 
